dcff4f5ff2a4b233b742d4c1b6a3de9e1fd886b7
tigasnogueira <contato.tigasnogueira@gmail.com>
2023-06-19 15:41:36 -0300

    Adicionando Model Filme

---
+++ Models/Filme.cs
@@ -0,0 +1,11 @@
+﻿namespace MinhaDemoMvc.Models;
+
+public class Filme
+{
+    public int Id { get; set; }
+    public string Titulo { get; set; }
+    public DateTime DataLancamento { get; set; }
+    public string Genero { get; set; }
+    public decimal Valor { get; set; }
+    public int Avaliacao { get; set; }
+}

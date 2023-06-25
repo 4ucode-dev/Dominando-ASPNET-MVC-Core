1a447fd332160b8f6ae0a0ba57af07c4d21419cc
Helyon-Pacheco <helyonftp@hotmail.com>
2023-06-24 21:04:44 -0300

    Criando e exibindo um View Component

---
+++ ViewComponents/CarrinhoViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MinhaDemoMvc.ViewComponents;
+
+[ViewComponent(Name = "Carrinho")]
+public class CarrinhoViewComponent : ViewComponent
+{
+    public int ItensCarrinho { get; set; }
+
+    public CarrinhoViewComponent()
+    {
+        ItensCarrinho = 3;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        return View(ItensCarrinho);
+    }
+}

986562c0fc87e14920ce779cb7f5a4ce8ca44837
tigasnogueira <contato.tigasnogueira@gmail.com>
2023-06-27 21:18:27 -0300

    Iniciando o Projeto MVC sem template

---
+++ src/DevIO.UI.Site/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.UI.Site.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}

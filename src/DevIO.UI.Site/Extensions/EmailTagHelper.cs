12e1136ff4f0c34cfebb96ae9cb635e490746740
tigasnogueira <contato.tigasnogueira@gmail.com>
2023-06-30 18:45:51 -0300

    Criando uma Custom TagHelper

---
+++ src/DevIO.UI.Site/Extensions/EmailTagHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace DevIO.UI.Site.Extensions;
+
+public class EmailTagHelper : TagHelper
+{
+    public string EmailDomain { get; set; } = "desenvolvedor.io";
+    override public async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+    {
+        var content = await output.GetChildContentAsync();
+        var target = content.GetContent() + "@" + EmailDomain;
+
+        output.TagName = "a";
+        output.Attributes.SetAttribute("href", $"mailto:{target}");
+        output.Content.SetContent(target);
+    }
+}

4dabd0f2330b95abab9c27a53478e03863e4c240
tigasnogueira <contato.tigasnogueira@gmail.com>
2023-07-09 20:50:54 -0300

    Injentando dependencias no MVC

--- src/DevIO.UI.Site/Controllers/HomeController.cs
+++ src/DevIO.UI.Site/Controllers/HomeController.cs
@@ -1,11 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DevIO.UI.Site.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DevIO.UI.Site.Controllers;
 
 public class HomeController : Controller
 {
+    private IPedidoRepository _pedidoRepository;
+
+    public HomeController(IPedidoRepository pedidoRepository)
+    {
+        _pedidoRepository = pedidoRepository;
+    }
+
+    //public IActionResult Index([FromServices] IPedidoRepository _pedidoRepository)
     public IActionResult Index()
     {
+        var pedido = _pedidoRepository.ObterPedido();
+
         return View();
     }
 }
